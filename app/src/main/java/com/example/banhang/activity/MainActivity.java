e00de64c0edacaba43833e0a744a41b23b0e2ec9
Thuy-Tine <ttttien495@gmail.com>
2026-05-22 01:43:58 +0700

    main

--- app/src/main/java/com/example/banhang/activity/MainActivity.java
+++ app/src/main/java/com/example/banhang/activity/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.banhang.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.banhang.R;
 
@@ -15,12 +13,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setContentView(R.layout.activity_main); // ⭐ THIẾU DÒNG NÀY
+
+        Button btnExplore = findViewById(R.id.btnExplore);
+
+        btnExplore.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
     }
 }
